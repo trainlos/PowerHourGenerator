67bf5d9ef9eb1571edffea8dfa3a3d655b2133f1
trainlos <tahoedude101@gmail.com>
2022-04-14 08:54:44 -0500

    Prioritize SDR formats

--- r.py
+++ r.py
@@ -86,6 +86,7 @@ def main():
     for i in range(1, 61):
         ydl_opts = {
             'format': 'bv[vcodec!=vp9]+ba',
+            'format_sort': ['hdr:sdr', 'quality', 'res', 'fps', 'source', 'codec'],
             'merge_output_format': 'mkv',
             'outtmpl': f'temp/orig/{i}',
             'ignoreerrors': 'True',
